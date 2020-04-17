9751d987bb3b3c7888a8aab5cb4eaa92234e1722
Aymeric De Abreu <ada@appouest.com>
2020-04-17 10:21:17 +0200

    Chnage DB model to add tweet<->user relationship

--- tweet.py
+++ tweet.py
@@ -1,20 +1,9 @@
 import datetime
 from application import db
 
-def createTweet():
-    author = input("Nom de l'auteur du tweet : ")
-    text = input("Contenu du tweet : ")
-    return Tweet(author, text)
-
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    authorName = db.Column(db.String(80), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     content = db.Column(db.String(140), nullable=False)
     image = db.Column(db.String(80), nullable=True)
     date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-
-    def print(self):
-        print("-------------------------")
-        print("@"+self.authorName + " said at "+str(self.date)+" :")
-        print(self.content)
-        print("-------------------------")
